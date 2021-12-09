b1c797c9a2dc91b07abc1c5a0a3f580c4dc9565d
Mike <michael.pursell@gmail.com>
2021-12-09 18:44:10 +0000

    added class for Item and adjusted logic

--- app/app.py
+++ app/app.py
@@ -11,49 +11,63 @@ class API_Handler():
 
     def __init__(self, category: str):
         self._api_key = os.environ.get('API_KEY')
+        # category is the category of image - 'apod' or 'Mars' as it stands
         self.category = category
         self.query = {}
         self.camera = 'mast'
     
         
     # choose an endpoint depending on whether "mars" or "apod" is passed in
-    # default to camera=mast
+    # default using a class attribute above to camera=mast
     def get_url(self):
+
         if self.category == 'apod':
             self.url = 'https://api.nasa.gov/planetary/apod'
             self.query = {'api_key': self._api_key}
-            return self.url, self.query
+            
+
         elif self.category == 'mars':
             self.url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
             self.query = {'sol':'1000', 'camera': f'{self.camera}', 'api_key':self._api_key}
 
-            return self.url, self.query
+        return self.url, self.query
 
+    
     def make_request(self) -> json:
         url = self.get_url()
         payload = self.query
-        #response = requests.get(f'{url}', params=payload)
         response = requests.get(self.url, params=payload)
         return response
+
+class Item:
+
+    def __init__(self):
+        self.image_url = ""
+        self.image_explanation = ""
+        self.image_date = ""
+        self.image_title =""
+        
 
 @app.route('/')
 def index():
 
-    #response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}")
-
     apiCall = API_Handler('apod')
     response = apiCall.make_request()
     
-    response = response.json()
-    url = response['url']
+    response = response.json() 
 
-    image_title = response['title']
-    image_explanation = response['explanation']
-    image_date = response['date']
+    # create new Item object and set a load of properties we'll want
+    item = Item()
+    item.image_url = response['url']
+    item.image_title = response['title']
+    item.image_explanation = response['explanation']
+    item.image_date = response['date']
+    
+    # now we've got an object with properties, we can just return that object,
+    # rather than a long list of vars.
+    return render_template('index.html', item=item)
 
-    return render_template('index.html', landing_image=url, image_title=image_title, image_explanation=image_explanation, image_date=image_date)
-
-@app.route('/mars', methods=['GET','POST'])
+@app.route('/mars', methods=['GET', 'POST'])
 def mars():
 
     camera = request.form.get('selected')
@@ -64,16 +78,20 @@ def mars():
     response = apiCall.make_request()
     response = response.json()
     
-    photos = response['photos']
+    item = Item()
+    item.photos = response['photos']
 
-
+    # reload after form submission, handle errors if there are no 
+    # images returned
     try:
-        image_url = photos[0]['img_src']
+        item.image_url = item.photos[0]['img_src']
     except IndexError:
-        response = requests.get(f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=mast&api_key={api_key}')
+        # make an API call with the class defaults (camera=mars) if there's nothing in the list of photos
+        apiCall = API_Handler('mars')
+        response = apiCall.make_request()
 
         response = response.json()
         photos = response['photos']
-        image_url = photos[0]['img_src']
+        item.image_url = item.photos[0]['img_src']
 
-    return render_template('mars.html', landing_image=image_url)
+    return render_template('mars.html', item=item)
